36a68c87686c73d26cc69e58b17f40a9efa86f40
Stannnnn <stann@live.nl>
2022-03-23 20:42:54 +0100

    fix yarn build, improved 01 - first libraries

--- TS/src/main.ts
+++ TS/src/main.ts
@@ -1,27 +1,40 @@
 import { Ascii } from 'core/01 - libraries/Ascii'
-import { BasicFunctions } from 'core/01 - libraries/Basic functions'
-import { DecodeString } from 'core/01 - libraries/Decode string'
-import { FunctionsOnNumbers } from 'core/01 - libraries/Functions on numbers'
-import { ColorCodes } from 'core/01 - libraries/Init colorCodes'
-import { Init_terrain_limit_variables } from 'core/01 - libraries/Init terrain limit variables'
+import { BasicFunctions } from 'core/01 - libraries/Basic_functions'
+import { DecodeString } from 'core/01 - libraries/Decode_string'
+import { FunctionsOnNumbers } from 'core/01 - libraries/Functions_on_numbers'
+import { ColorCodes } from 'core/01 - libraries/Init_colorCodes'
+import { Init_terrain_limit_variables } from 'core/01 - libraries/Init_terrain_limit_variables'
 import { Text } from 'core/01 - libraries/Text'
 import { ZLibrary } from 'core/01 - libraries/ZLibrary'
+import { ViewAllHideAll } from 'core/03 - view all hide all/View_all_hide_all'
 import { errorHandler } from 'Utils/mapUtils'
 import { addScriptHook, W3TS_HOOK } from 'w3ts/hooks'
 
 const tsMain = () => {
     // call main game func
 
-    // Order matters, text is required by most
-    Text()
-    Ascii()
-    BasicFunctions()
-    // Constants()
-    DecodeString()
-    FunctionsOnNumbers()
-    ColorCodes()
-    Init_terrain_limit_variables()
-    ZLibrary()
+    // 1
+    {
+        // Order matters, text is required by most
+        Text()
+        Ascii()
+        BasicFunctions()
+        // Constants()
+        DecodeString()
+        FunctionsOnNumbers()
+        ColorCodes()
+        Init_terrain_limit_variables()
+        ZLibrary()
+    }
+
+    // 2
+    {
+    }
+
+    // 3
+    {
+        ViewAllHideAll()
+    }
 }
 
 addScriptHook(W3TS_HOOK.MAIN_AFTER, errorHandler(tsMain))
