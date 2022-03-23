e46d68a3a1887d0314d8e286537258355b3e84a2
Stannnnn <stann@live.nl>
2022-03-23 20:55:39 +0100

    Reanmes

--- TS/src/main.ts
+++ TS/src/main.ts
@@ -6,7 +6,7 @@ import { ColorCodes } from 'core/01_libraries/Init_colorCodes'
 import { Init_terrain_limit_variables } from 'core/01_libraries/Init_terrain_limit_variables'
 import { Text } from 'core/01_libraries/Text'
 import { ZLibrary } from 'core/02_bibliotheques_externes/ZLibrary'
-import { ViewAllHideAll } from 'core/03_view all hide all/View_all_hide_all'
+import { ViewAllHideAll } from 'core/03_view_all_hide_all/View_all_hide_all'
 import { errorHandler } from 'Utils/mapUtils'
 import { addScriptHook, W3TS_HOOK } from 'w3ts/hooks'
 
