156b3f65dc4e7a56e60ddd1bd5a479ef95c117d1
Max <maxime.seeweb@gmail.com>
2022-03-26 18:43:44 +0100

    script to get data from a file text made by the game

---
+++ bin/getStringFromMap.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Créé avec PhpStorm.
+ * Auteur: Max
+ * Date: 26/03/2022
+ * Heure: 00:57
+ */
+
+
+
+const DATA_FILE = __DIR__."/mec_data.txt";
+
+
+if(!is_file(DATA_FILE)){
+    die("File \"mec_data.txt\" missing");
+}
+
+
+$content = file_get_contents(DATA_FILE);
+
+
+
+if(preg_match('/call BlzSendSyncData\(".*?","(.+?)"/', $content, $match)){
+    $base64 = $match[1];
+    $uncodedContent = base64_decode($base64);
+    $finalString = preg_replace('/^.+?{/', '{', $uncodedContent);
+
+    echo $finalString;
+}else{
+    echo "no content found";
+}
