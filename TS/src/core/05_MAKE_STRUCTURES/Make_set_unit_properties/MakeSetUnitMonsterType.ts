768c74ac92d82e0451dca18abf8a4818a5a62f88
Max <maxime.seeweb@gmail.com>
2022-03-29 23:35:44 +0200

    new Make converts

--- TS/src/core/05_MAKE_STRUCTURES/Make_set_unit_properties/MakeSetUnitMonsterType.ts
+++ TS/src/core/05_MAKE_STRUCTURES/Make_set_unit_properties/MakeSetUnitMonsterType.ts
@@ -1,93 +1,52 @@
-import { Make } from '../Make/Make'
+import { MakeOneByOneOrTwoClicks } from 'core/05_MAKE_STRUCTURES/Make/MakeOneByOneOrTwoClicks'
+import {MonsterType} from "../../04_STRUCTURES/Monster/MonsterType";
+import {Monster} from "../../04_STRUCTURES/Monster/Monster";
+import {Text} from "../../01_libraries/Text";
 
-export class MakeSetUnitMonsterType extends Make {
-    lastX: real
-    lastY: real
-    private lastLocIsSaved: boolean
-    private lastLocSavedIsUsed: boolean
-    private unitLastClic: unit
-    private mode: string
+export class MakeSetUnitMonsterType extends MakeOneByOneOrTwoClicks {
     private mt: MonsterType
+
+    constructor(maker: unit, mode: string, mt: MonsterType) {
+        super(maker, 'setUnitMonsterType', mode)
+        this.mt = mt
+    }
 
     getMonsterType = (): MonsterType => {
         return this.mt
     }
-
-    isLastLocSavedUsed = (): boolean => {
-        return this.lastLocSavedIsUsed
-    }
-
-    // TODO; Used to be static
-    create = (maker: unit, mode: string, mt: MonsterType): MakeSetUnitMonsterType => {
+    
+    doActions() {
         //modes : oneByOne, twoClics
-        let m: MakeSetUnitMonsterType
-        if (maker === null || (mode !== 'oneByOne' && mode !== 'twoClics') || mt === 0) {
-            return 0
-        }
-        m = MakeSetUnitMonsterType.allocate()
-        m.maker = maker
-        m.makerOwner = GetOwningPlayer(maker)
-        m.kind = 'setUnitMonsterType'
-        m.mode = mode
-        m.mt = mt
-        m.lastLocIsSaved = false
-        m.lastLocSavedIsUsed = false
-        m.t = CreateTrigger()
-        TriggerAddAction(m.t, Make_GetActions(m.kind))
-        TriggerRegisterUnitEvent(m.t, m.maker, EVENT_UNIT_ISSUED_POINT_ORDER)
-        return m
-    }
+        let nbMonstersFixed = 0
 
-    destroy = () => {
-        DestroyTrigger(this.t)
-        this.t = null
-        this.maker = null
-        RemoveUnit(this.unitLastClic)
-        this.unitLastClic = null
-    }
-
-    saveLoc = (x: number, y: number) => {
-        this.lastX = x
-        this.lastY = y
-        this.lastLocIsSaved = true
-        this.lastLocSavedIsUsed = true
-        if (this.unitLastClic === null) {
-            this.unitLastClic = CreateUnit(this.makerOwner, MAKE_LAST_CLIC_UNIT_ID, x, y, GetRandomDirectionDeg())
+        if (this.getMode() == 'oneByOne') {
+            let monster = this.escaper.getMakingLevel().getMonsterNear(this.orderX, this.orderY)
+            if (monster) {
+                if (monster.setMonsterType(this.getMonsterType())) {
+                    nbMonstersFixed = 1
+                }
+            }
         } else {
-            SetUnitX(this.unitLastClic, x)
-            SetUnitY(this.unitLastClic, y)
+            //mode twoClics
+            if (!this.isLastLocSavedUsed()) {
+                this.saveLoc(this.orderX, this.orderY)
+                return
+            }
+
+            //todomax getMonsters method in Level class to union all types of Monsters in an array of Monster
+            let monsters = escaper.getMakingLevel().getMonstersBetweenLocs(this.orderX, this.orderY)
+            monsters.map(monster => {
+                if(monster.setMonsterType(this.getMonsterType())){
+                    nbMonstersFixed++
+                }
+            })
         }
-        EscaperFunctions.Hero2Escaper(this.maker).destroyCancelledActions()
-    }
 
-    unsaveLoc = (): boolean => {
-        if (!this.lastLocSavedIsUsed) {
-            return false
+        if (nbMonstersFixed <= 1) {
+            Text.mkP(this.makerOwner, I2S(nbMonstersFixed) + ' monster fixed.')
+        } else {
+            Text.mkP(this.makerOwner, I2S(nbMonstersFixed) + ' monsters fixed.')
         }
-        RemoveUnit(this.unitLastClic)
-        this.unitLastClic = null
-        this.lastLocSavedIsUsed = false
-        return true
-    }
-
-    unsaveLocDefinitely = () => {
-        this.unsaveLoc()
-        this.lastLocIsSaved = false
-    }
-
-    cancelLastAction = (): boolean => {
-        return this.unsaveLoc()
-    }
-
-    redoLastAction = (): boolean => {
-        if (this.lastLocIsSaved && !this.lastLocSavedIsUsed) {
-            this.saveLoc(this.lastX, this.lastY)
-            return true
-        }
-        return false
-    }
-
-    getMode = (): string => {
-        return this.mode
+        this.unsaveLocDefinitely()
     }
 }
--- TS/src/core/05_MAKE_STRUCTURES/Make_set_unit_properties/MakeSetUnitTeleportPeriod.ts
+++ TS/src/core/05_MAKE_STRUCTURES/Make_set_unit_properties/MakeSetUnitTeleportPeriod.ts
@@ -1,98 +1,53 @@
-import { Make } from '../Make/Make'
+import { MakeOneByOneOrTwoClicks } from 'core/05_MAKE_STRUCTURES/Make/MakeOneByOneOrTwoClicks'
+import {BasicFunctions} from "../../01_libraries/Basic_functions";
+import {Text} from "../../01_libraries/Text";
 
-export class MakeSetUnitTeleportPeriod extends Make {
-    lastX: real
-    lastY: real
-    private lastLocIsSaved: boolean
-    private lastLocSavedIsUsed: boolean
-    private unitLastClic: unit
-    private mode: string
-    private period: real
+export class MakeSetUnitTeleportPeriod extends MakeOneByOneOrTwoClicks {
+    private period: number
+
+    constructor(maker: unit, mode: string, period: number) {
+        super(maker, 'setUnitTeleportPeriod', mode)
+        
+        if (period < MONSTER_TELEPORT_PERIOD_MIN || period > MONSTER_TELEPORT_PERIOD_MAX) {
+            throw this.constructor.name + " : wrong period \"" + period + "\""
+        }
+        
+        this.period = period
+    }
 
     getPeriod = (): number => {
         return this.period
     }
+    
+    doActions() {
+        let nbMonstersFixed = 0
 
-    isLastLocSavedUsed = (): boolean => {
-        return this.lastLocSavedIsUsed
-    }
-
-    // TODO; Used to be static
-    create = (maker: unit, mode: string, period: number): MakeSetUnitTeleportPeriod => {
-        //modes : oneByOne, twoClics
-        let m: MakeSetUnitTeleportPeriod
-        if (
-            maker === null ||
-            (mode !== 'oneByOne' && mode !== 'twoClics') ||
-            period < MONSTER_TELEPORT_PERIOD_MIN ||
-            period > MONSTER_TELEPORT_PERIOD_MAX
-        ) {
-            return 0
-        }
-        m = MakeSetUnitTeleportPeriod.allocate()
-        m.maker = maker
-        m.makerOwner = GetOwningPlayer(maker)
-        m.kind = 'setUnitTeleportPeriod'
-        m.mode = mode
-        m.period = period
-        m.lastLocIsSaved = false
-        m.lastLocSavedIsUsed = false
-        m.t = CreateTrigger()
-        TriggerAddAction(m.t, Make_GetActions(m.kind))
-        TriggerRegisterUnitEvent(m.t, m.maker, EVENT_UNIT_ISSUED_POINT_ORDER)
-        return m
-    }
-
-    destroy = () => {
-        DestroyTrigger(this.t)
-        this.t = null
-        this.maker = null
-        RemoveUnit(this.unitLastClic)
-        this.unitLastClic = null
-    }
-
-    saveLoc = (x: number, y: number) => {
-        this.lastX = x
-        this.lastY = y
-        this.lastLocIsSaved = true
-        this.lastLocSavedIsUsed = true
-        if (this.unitLastClic === null) {
-            this.unitLastClic = CreateUnit(this.makerOwner, MAKE_LAST_CLIC_UNIT_ID, x, y, GetRandomDirectionDeg())
+        if (this.getMode() == 'oneByOne') {
+            let monsterTP = this.escaper.getMakingLevel().monstersTeleport.getMonsterNear(this.orderX, this.orderY)
+            if (monsterTP) {
+                monsterTP.setPeriod(this.getPeriod())
+                nbMonstersFixed = 1
+            }
         } else {
-            SetUnitX(this.unitLastClic, x)
-            SetUnitY(this.unitLastClic, y)
+            //mode twoClics
+            if (!this.isLastLocSavedUsed()) {
+                this.saveLoc(this.orderX, this.orderY)
+                return
+            }
+
+            //todomax make all Monster<SpecificType>Array extend a new abstract class MonsterArray
+            let monstersTP = this.escaper.getMakingLevel().monstersTeleport.getMonstersBetweenLocs(this.orderX, this.orderY)
+            monstersTP.map(monsterTP => {
+                monsterTP.setPeriod(this.getPeriod())
+                nbMonstersFixed++
+            })
         }
-        EscaperFunctions.Hero2Escaper(this.maker).destroyCancelledActions()
-    }
 
-    unsaveLoc = (): boolean => {
-        if (!this.lastLocSavedIsUsed) {
-            return false
+        if (nbMonstersFixed <= 1) {
+            Text.mkP(this.makerOwner, I2S(nbMonstersFixed) + ' monster fixed.')
+        } else {
+            Text.mkP(this.makerOwner, I2S(nbMonstersFixed) + ' monsters fixed.')
         }
-        RemoveUnit(this.unitLastClic)
-        this.unitLastClic = null
-        this.lastLocSavedIsUsed = false
-        return true
-    }
-
-    unsaveLocDefinitely = () => {
-        this.unsaveLoc()
-        this.lastLocIsSaved = false
-    }
-
-    cancelLastAction = (): boolean => {
-        return this.unsaveLoc()
-    }
-
-    redoLastAction = (): boolean => {
-        if (this.lastLocIsSaved && !this.lastLocSavedIsUsed) {
-            this.saveLoc(this.lastX, this.lastY)
-            return true
-        }
-        return false
-    }
-
-    getMode = (): string => {
-        return this.mode
+        this.unsaveLocDefinitely()
     }
 }
