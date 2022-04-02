922c0ae85c12c06ab3a8e0adea1af9e802f2646e
Max <maxime.seeweb@gmail.com>
2022-04-02 18:13:36 +0200

    Terrain_functions.ts and Exchange_terrains.ts : modify exports

--- TS/src/core/07_TRIGGERS/Modify_terrain_Functions/Terrain_functions.ts
+++ TS/src/core/07_TRIGGERS/Modify_terrain_Functions/Terrain_functions.ts
@@ -1,132 +1,115 @@
-import { NB_MAX_OF_TERRAINS, RED, TERRAIN_DATA_DISPLAY_TIME } from 'core/01_libraries/Constants'
+import {NB_MAX_OF_TERRAINS, RED, TERRAIN_DATA_DISPLAY_TIME} from 'core/01_libraries/Constants'
 import {
     COLOR_TERRAIN_DEATH,
     COLOR_TERRAIN_SLIDE,
     COLOR_TERRAIN_WALK,
     udg_colorCode,
 } from 'core/01_libraries/Init_colorCodes'
-import { TerrainType } from 'core/04_STRUCTURES/TerrainType/TerrainType'
-import { Globals } from 'core/09_From_old_Worldedit_triggers/globals_variables_and_triggers'
-import { TerrainTypeFunctions } from '../../04_STRUCTURES/TerrainType/Terrain_type_functions'
-import { TerrainTypeMax } from './Terrain_type_max'
-import { TerrainTypeNamesAndData } from './Terrain_type_names_and_data'
+import {TerrainType} from 'core/04_STRUCTURES/TerrainType/TerrainType'
+import {Globals} from 'core/09_From_old_Worldedit_triggers/globals_variables_and_triggers'
+import {TerrainTypeMax} from './Terrain_type_max'
+import {TerrainTypeNamesAndData} from './Terrain_type_names_and_data'
+import {TerrainTypeId2TerrainType} from "../../04_STRUCTURES/TerrainType/Terrain_type_functions";
 
-const initTerrainFunctions = () => {
-    const IsTerrainAlreadyUsed = (terrainType: number): boolean => {
-        let i = 0
-        while (true) {
-            if (Globals.udg_used_terrain_types[i] === 0) break
-            if (Globals.udg_used_terrain_types[i] === terrainType) {
-                return true
-            }
-            i = i + 1
-        }
-        return false
-    }
-
-    const IsTerrainsLimitNumberReached = (): boolean => {
-        return Globals.udg_nb_used_terrains === NB_MAX_OF_TERRAINS
-    }
-
-    const AddNewTerrain = (newTerrain: number): boolean => {
-        if (IsTerrainsLimitNumberReached() || IsTerrainAlreadyUsed(newTerrain)) {
-            return false
-        }
-        Globals.udg_used_terrain_types[Globals.udg_nb_used_terrains] = newTerrain
-        Globals.udg_nb_used_terrains = Globals.udg_nb_used_terrains + 1
-        return true
-    }
-
-    const CanUseTerrain = (terrainType: number): boolean => {
-        if (IsTerrainAlreadyUsed(terrainType)) {
+export const IsTerrainAlreadyUsed = (terrainType: number): boolean => {
+    let i = 0
+    while (true) {
+        if (Globals.udg_used_terrain_types[i] === 0) break
+        if (Globals.udg_used_terrain_types[i] === terrainType) {
             return true
         }
-        return AddNewTerrain(terrainType)
+        i = i + 1
     }
-
-    const GetRandomTerrain = (): number => {
-        return TerrainTypeMax.TerrainTypeMaxId2TerrainTypeId(GetRandomInt(1, TerrainTypeNamesAndData.NB_TERRAINS_TOTAL))
-    }
-
-    const GetRandomUsedTerrain = (): number => {
-        return Globals.udg_used_terrain_types[GetRandomInt(0, Globals.udg_nb_used_terrains - 1)]
-    }
-
-    const GetRandomNotUsedTerrain = (): number => {
-        let terrainType: number
-        while (true) {
-            terrainType = GetRandomTerrain()
-            if (!IsTerrainAlreadyUsed(terrainType)) break
-        }
-        return terrainType
-    }
-
-    const GetTerrainName = (terrain: number) => {
-        if (terrain > TerrainTypeNamesAndData.NB_TERRAINS_TOTAL) {
-            return TerrainTypeNamesAndData.TERRAIN_TYPE_NAMES[TerrainTypeMax.TerrainTypeId2TerrainTypeMaxId(terrain)]
-        }
-
-        if (terrain <= 0) {
-            return null
-        }
-
-        return TerrainTypeNamesAndData.TERRAIN_TYPE_NAMES[terrain]
-    }
-
-    const GetTerrainData = (terrain: number) => {
-        //GetTerrainData('Nice') == "46 : Northrend - Glace    'Nice'"
-        let str: string
-        let maxId: number
-        let terrainType: TerrainType
-
-        if (terrain > TerrainTypeNamesAndData.NB_TERRAINS_TOTAL) {
-            maxId = TerrainTypeMax.TerrainTypeId2TerrainTypeMaxId(terrain)
-        } else {
-            if (terrain > 0) {
-                maxId = terrain
-                terrain = TerrainTypeMax.TerrainTypeMaxId2TerrainTypeId(maxId)
-            } else {
-                return null
-            }
-        }
-        str = udg_colorCode[RED] + TerrainTypeNamesAndData.TERRAIN_TYPE_DATA[maxId]
-
-        terrainType = TerrainTypeFunctions.TerrainTypeId2TerrainType(terrain)
-        if (terrainType !== null) {
-            if (terrainType.getKind() == 'slide') {
-                str = str + COLOR_TERRAIN_SLIDE
-            } else {
-                if (terrainType.getKind() == 'walk') {
-                    str = str + COLOR_TERRAIN_WALK
-                } else {
-                    str = str + COLOR_TERRAIN_DEATH
-                }
-            }
-            str = str + '        ' + terrainType.label
-            if (terrainType.theAlias != null) {
-                str = str + '  ' + terrainType.theAlias
-            }
-        }
-
-        return str
-    }
-
-    const DisplayTerrainDataToPlayer = (p: player, terrain: number) => {
-        DisplayTimedTextToPlayer(p, 0, 0, TERRAIN_DATA_DISPLAY_TIME, TerrainFunctions.GetTerrainData(terrain)!)
-    }
-
-    return {
-        IsTerrainAlreadyUsed,
-        IsTerrainsLimitNumberReached,
-        AddNewTerrain,
-        CanUseTerrain,
-        GetRandomTerrain,
-        GetRandomUsedTerrain,
-        GetRandomNotUsedTerrain,
-        GetTerrainName,
-        GetTerrainData,
-        DisplayTerrainDataToPlayer,
-    }
+    return false
 }
 
-export const TerrainFunctions = initTerrainFunctions()
+export const IsTerrainsLimitNumberReached = (): boolean => {
+    return Globals.udg_nb_used_terrains === NB_MAX_OF_TERRAINS
+}
+
+export const AddNewTerrain = (newTerrain: number): boolean => {
+    if (IsTerrainsLimitNumberReached() || IsTerrainAlreadyUsed(newTerrain)) {
+        return false
+    }
+    Globals.udg_used_terrain_types[Globals.udg_nb_used_terrains] = newTerrain
+    Globals.udg_nb_used_terrains = Globals.udg_nb_used_terrains + 1
+    return true
+}
+
+export const CanUseTerrain = (terrainType: number): boolean => {
+    if (IsTerrainAlreadyUsed(terrainType)) {
+        return true
+    }
+    return AddNewTerrain(terrainType)
+}
+
+export const GetRandomTerrain = (): number => {
+    return TerrainTypeMax.TerrainTypeMaxId2TerrainTypeId(GetRandomInt(1, TerrainTypeNamesAndData.NB_TERRAINS_TOTAL))
+}
+
+export const GetRandomUsedTerrain = (): number => {
+    return Globals.udg_used_terrain_types[GetRandomInt(0, Globals.udg_nb_used_terrains - 1)]
+}
+
+export const GetRandomNotUsedTerrain = (): number => {
+    let terrainType: number
+    while (true) {
+        terrainType = GetRandomTerrain()
+        if (!IsTerrainAlreadyUsed(terrainType)) break
+    }
+    return terrainType
+}
+
+export const GetTerrainName = (terrain: number) => {
+    if (terrain > TerrainTypeNamesAndData.NB_TERRAINS_TOTAL) {
+        return TerrainTypeNamesAndData.TERRAIN_TYPE_NAMES[TerrainTypeMax.TerrainTypeId2TerrainTypeMaxId(terrain)]
+    }
+
+    if (terrain <= 0) {
+        return null
+    }
+
+    return TerrainTypeNamesAndData.TERRAIN_TYPE_NAMES[terrain]
+}
+
+export const GetTerrainData = (terrain: number) => {
+    //GetTerrainData('Nice') == "46 : Northrend - Glace    'Nice'"
+    let str: string
+    let maxId: number
+    let terrainType: TerrainType | null
+
+    if (terrain > TerrainTypeNamesAndData.NB_TERRAINS_TOTAL) {
+        maxId = TerrainTypeMax.TerrainTypeId2TerrainTypeMaxId(terrain)
+    } else {
+        if (terrain > 0) {
+            maxId = terrain
+            terrain = TerrainTypeMax.TerrainTypeMaxId2TerrainTypeId(maxId)
+        } else {
+            return null
+        }
+    }
+    str = udg_colorCode[RED] + TerrainTypeNamesAndData.TERRAIN_TYPE_DATA[maxId]
+
+    terrainType = TerrainTypeId2TerrainType(terrain)
+    if (terrainType) {
+        if (terrainType.getKind() == 'slide') {
+            str = str + COLOR_TERRAIN_SLIDE
+        } else {
+            if (terrainType.getKind() == 'walk') {
+                str = str + COLOR_TERRAIN_WALK
+            } else {
+                str = str + COLOR_TERRAIN_DEATH
+            }
+        }
+        str = str + '        ' + terrainType.label
+        if (terrainType.theAlias != null) {
+            str = str + '  ' + terrainType.theAlias
+        }
+    }
+
+    return str
+}
+
+export const DisplayTerrainDataToPlayer = (p: player, terrain: number) => {
+    DisplayTimedTextToPlayer(p, 0, 0, TERRAIN_DATA_DISPLAY_TIME, GetTerrainData(terrain)!)
+}
--- TS/src/core/07_TRIGGERS/Triggers_to_modify_terrains/Exchange_terrains.ts
+++ TS/src/core/07_TRIGGERS/Triggers_to_modify_terrains/Exchange_terrains.ts
@@ -1,72 +1,62 @@
-import { Constants, LARGEUR_CASE } from 'core/01_libraries/Constants'
-import { Text } from 'core/01_libraries/Text'
-import { TerrainType } from 'core/04_STRUCTURES/TerrainType/TerrainType'
-import { udg_terrainTypes } from '../../../../globals'
-import { Modify_terrain_functions } from '../Modify_terrain_Functions/Modify_terrain_functions'
-import { TerrainModifyingTrig } from './Terrain_modifying_trig'
+import {Constants, LARGEUR_CASE} from 'core/01_libraries/Constants'
+import {Text} from 'core/01_libraries/Text'
+import {TerrainType} from 'core/04_STRUCTURES/TerrainType/TerrainType'
+import {udg_terrainTypes} from '../../../../globals'
+import {Modify_terrain_functions} from '../Modify_terrain_Functions/Modify_terrain_functions'
+import {TerrainModifyingTrig} from './Terrain_modifying_trig'
 
-const initExchangeTerrains = () => {
-    let terrainA: number
-    let terrainB: number
-    let terrainModifyWorking = false
+let terrainA: number
+let terrainB: number
+let terrainModifyWorking = false
 
-    const StartTerrainModifying = () => {
-        TerrainModifyingTrig.StopEnabledCheckTerrainTriggers()
-        TriggerClearActions(TerrainModifyingTrig.gg_trg_Terrain_modifying_trig)
-        TriggerAddAction(TerrainModifyingTrig.gg_trg_Terrain_modifying_trig, () => {
-            let x: number
-            let terrainType: number
-            //local integer i = 1
-            //loop
-            //exitwhen (i > TERRAIN_MODIFYING_NB_LINES_TO_DO)
-            x = Constants.MAP_MIN_X
-            while (true) {
-                if (x > Constants.MAP_MAX_X) break
-                terrainType = GetTerrainType(x, y)
-                if (terrainType === terrainA) {
-                    Modify_terrain_functions.ChangeTerrainType(x, y, terrainB)
-                } else {
-                    if (terrainType === terrainB) {
-                        Modify_terrain_functions.ChangeTerrainType(x, y, terrainA)
-                    }
+const StartTerrainModifying = () => {
+    TerrainModifyingTrig.StopEnabledCheckTerrainTriggers()
+    TriggerClearActions(TerrainModifyingTrig.gg_trg_Terrain_modifying_trig)
+    TriggerAddAction(TerrainModifyingTrig.gg_trg_Terrain_modifying_trig, () => {
+        let x: number
+        let terrainType: number
+
+        x = Constants.MAP_MIN_X
+        while (true) {
+            if (x > Constants.MAP_MAX_X) break
+            terrainType = GetTerrainType(x, y)
+            if (terrainType === terrainA) {
+                Modify_terrain_functions.ChangeTerrainType(x, y, terrainB)
+            } else {
+                if (terrainType === terrainB) {
+                    Modify_terrain_functions.ChangeTerrainType(x, y, terrainA)
                 }
-                x = x + LARGEUR_CASE
             }
-            y = y + LARGEUR_CASE
-            if (y > Constants.MAP_MAX_Y) {
-                DisableTrigger(GetTriggeringTrigger())
-                TerrainModifyingTrig.RestartEnabledCheckTerrainTriggers()
-                terrainModifyWorking = false
-                return
-            }
-            //i = i + 1
-            //endloop
-        })
-        let y = Constants.MAP_MIN_Y
-        EnableTrigger(TerrainModifyingTrig.gg_trg_Terrain_modifying_trig)
-        terrainModifyWorking = true
-    }
-
-    const ExchangeTerrains = (terrainTypeLabelA: string, terrainTypeLabelB: string): boolean => {
-        const terrainTypeA: TerrainType = udg_terrainTypes.get(terrainTypeLabelA)
-        const terrainTypeB: TerrainType = udg_terrainTypes.get(terrainTypeLabelB)
-        if (terrainTypeA === terrainTypeB || terrainTypeA === 0 || terrainTypeB === 0) {
-            return false
+            x = x + LARGEUR_CASE
         }
-        if (terrainModifyWorking) {
-            Text.erA("can't execute two commands of this type simultaneously !")
-            return false
+        y = y + LARGEUR_CASE
+        if (y > Constants.MAP_MAX_Y) {
+            DisableTrigger(GetTriggeringTrigger())
+            TerrainModifyingTrig.RestartEnabledCheckTerrainTriggers()
+            terrainModifyWorking = false
+            return
         }
-        terrainA = terrainTypeA.getTerrainTypeId()
-        terrainB = terrainTypeB.getTerrainTypeId()
-
-        StartTerrainModifying()
-        terrainTypeA.setTerrainTypeId(terrainB)
-        terrainTypeB.setTerrainTypeId(terrainA)
-        return true
-    }
-
-    return { ExchangeTerrains }
+    })
+    let y = Constants.MAP_MIN_Y
+    EnableTrigger(TerrainModifyingTrig.gg_trg_Terrain_modifying_trig)
+    terrainModifyWorking = true
 }
 
-export const ExchangeTerrains = initExchangeTerrains()
+export const ExchangeTerrains = (terrainTypeLabelA: string, terrainTypeLabelB: string): boolean => {
+    const terrainTypeA: TerrainType = udg_terrainTypes.get(terrainTypeLabelA)
+    const terrainTypeB: TerrainType = udg_terrainTypes.get(terrainTypeLabelB)
+    if (terrainTypeA === terrainTypeB || !terrainTypeA || !terrainTypeB) {
+        return false
+    }
+    if (terrainModifyWorking) {
+        Text.erA("can't execute two commands of this type simultaneously !")
+        return false
+    }
+    terrainA = terrainTypeA.getTerrainTypeId()
+    terrainB = terrainTypeB.getTerrainTypeId()
+
+    StartTerrainModifying()
+    terrainTypeA.setTerrainTypeId(terrainB)
+    terrainTypeB.setTerrainTypeId(terrainA)
+    return true
+}
