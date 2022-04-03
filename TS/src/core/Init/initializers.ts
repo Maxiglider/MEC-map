b856fbee6a29f22ea472781da4ccd069afd81937
Max <maxime.seeweb@gmail.com>
2022-04-03 21:49:06 +0200

    activate view all and no selection circle

--- TS/src/core/Init/initializers.ts
+++ TS/src/core/Init/initializers.ts
@@ -1,8 +1,9 @@
 import {init_StartAndEnd} from "../04_STRUCTURES/Level/StartAndEnd";
+import {initViewAllHideAll} from "../03_view_all_hide_all/View_all_hide_all";
+import {initNoSelectionCircle} from "../08_GAME/Init_game/No_selection_circle";
 
 export const initializers = () => {
     init_StartAndEnd()
-    // init_globals()
-    // initViewAllHideAll()
-    // initNoSelectionCircle()
+    initViewAllHideAll()
+    initNoSelectionCircle()
 }
