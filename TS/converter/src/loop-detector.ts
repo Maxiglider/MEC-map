dbfa9a4fbef79393a1c14109bd8027b8e79bae7d
Stannnnn <stann@live.nl>
2022-04-04 17:17:51 +0200

    Even more loops

--- TS/converter/src/loop-detector.ts
+++ TS/converter/src/loop-detector.ts
@@ -6,7 +6,7 @@ const targetFile = resolve('./dist/map.w3x/war3map.lua')
 let contents = readFileSync(targetFile).toString()
 let counter = 0
 
-contents = contents.replace(new RegExp('while (.*) do', 'gmi'), (_, a) => {
+contents = contents.replace(new RegExp('while ([\\s\\S]*?) do', 'gmi'), (_, a) => {
     return `while ${a} do\nprint("Loop: #${counter++}")\n`
 })
 
