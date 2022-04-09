9f1483bda226f836b803be35aafea87a6adc4899
Max <maxime.seeweb@gmail.com>
2022-04-09 18:06:27 +0200

    improving array handler

--- TS/src/Utils/ArrayHandler.ts
+++ TS/src/Utils/ArrayHandler.ts
@@ -1,39 +1,59 @@
 export class ArrayHandler {
-    private static tArrays: any[] = []
+    private static tArrays = new Map<number, any>()
     private static freedArrays: any[] = []
 
     public static getNewArray(){
-        const len = ArrayHandler.tArrays.length;
-        let arrHandler = ArrayHandler.freedArrays.shift()
-        if(arrHandler){
-            ArrayHandler.tArrays[len] = arrHandler
-            return arrHandler
+        let arr: any[] = ArrayHandler.freedArrays.shift()
+        let index
+
+        if(arr){
+            index = (getmetatable(arr) as any).__id
         }else{
-            return ArrayHandler.tArrays[len] = []
+            arr = []
+            index = ArrayHandler.tArrays.size;
+            const meta: any = { __id: index }
+            setmetatable(arr, meta)
         }
+
+        ArrayHandler.tArrays.set(index, arr)
+        return arr
     }
 
     public static clearArray(arr: any[]){
         //remove from tArrays
-        const len = ArrayHandler.tArrays.length;
-        for(let i = 0; i <= len; i++){
-            if(arr === ArrayHandler.tArrays[i]){
-                //delete content of the array
-                const arrLen = arr.length
-                for(let j = 0; j < arrLen; j++){
-                    arr[j] = null
-                }
-                arr.length = 0
+        const index = (getmetatable(arr) as any).__id
 
-                //remove the array from the active ones
-                table.remove(arr, i)
+        const arrayInMap = ArrayHandler.tArrays.get(index)
 
-                //add the array to the freed ones
-                const freedLen = ArrayHandler.freedArrays.length
-                ArrayHandler.freedArrays[freedLen] = arr
-
-                return
-            }
+        if(!arrayInMap){
+            throw "Index \"" + index + "\" not known in ArrayHandler"
         }
+
+        if(arrayInMap !== arr){
+            throw "Arrays different in ArrayHandler"
+        }
+
+        //delete content of the array
+        const arrLen = arr.length
+        for (let j = 0; j < arrLen; j++) {
+            arr[j] = null
+        }
+        arr.length = 0
+
+        //remove the array from the active ones
+        ArrayHandler.tArrays.delete(index)
+
+        //add the array to the freed ones
+        const freedLen = ArrayHandler.freedArrays.length
+        ArrayHandler.freedArrays[freedLen] = arr
+    }
+
+    public static clearArrayOfArray(arr: any[]){
+        const len = arr.length
+        for(let i = 0; i < len; i++){
+            ArrayHandler.clearArray(arr[i])
+        }
+
+        ArrayHandler.clearArray(arr)
     }
 }
