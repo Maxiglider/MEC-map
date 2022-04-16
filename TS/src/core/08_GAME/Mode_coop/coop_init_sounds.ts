e8a7c9362fe7df032c5390e372746e8595dead55
Stannnnn <stann@live.nl>
2022-04-16 17:34:28 +0200

    Added not working SoundUtils

--- TS/src/core/08_GAME/Mode_coop/coop_init_sounds.ts
+++ TS/src/core/08_GAME/Mode_coop/coop_init_sounds.ts
@@ -1,3 +1,3 @@
-const COOP_DUREE_SON = 280
-export const udg_coop_index_son = null //DefineSound('war3mapImported\\goutte.wav', COOP_DUREE_SON, false, true)
+export const udg_coop_index_son = null //DefineSound('war3mapImported\\goutte.wav', 280, false, true)
 //todomax make sound work
+// todo; use SoundPlay3DUnit('war3mapImported\\goutte.wav', 280)
