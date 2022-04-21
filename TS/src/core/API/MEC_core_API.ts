91373d8ecc515703ff3706922f774bdd6fc78ff1
Stannnnn <stann@live.nl>
2022-04-21 17:29:33 +0200

    Fix types

--- TS/src/core/API/MEC_core_API.ts
+++ TS/src/core/API/MEC_core_API.ts
@@ -1,9 +1,9 @@
-import {LoadMapFromCache} from "../07_TRIGGERS/Load_map_from_gamecache/LoadMapFromCache";
-import {errorHandler} from "../../Utils/mapUtils";
-import {makingRightsToAll} from "../06_COMMANDS/Rights/manage_rights";
-import {getUdgEscapers, getUdgLevels, getUdgMonsterTypes, getUdgTerrainTypes} from "../../../globals";
-import {hooks} from "./GeneralHooks";
-import {MonsterSimplePatrol} from "../04_STRUCTURES/Monster/MonsterSimplePatrol";
+import { getUdgEscapers, getUdgLevels, getUdgMonsterTypes, getUdgTerrainTypes } from '../../../globals'
+import { errorHandler } from '../../Utils/mapUtils'
+import { MonsterSimplePatrol } from '../04_STRUCTURES/Monster/MonsterSimplePatrol'
+import { makingRightsToAll } from '../06_COMMANDS/Rights/manage_rights'
+import { LoadMapFromCache } from '../07_TRIGGERS/Load_map_from_gamecache/LoadMapFromCache'
+import { hooks } from './GeneralHooks'
 
 export const MEC_core_API = {
     setGameData: (jsonString: string) => {
@@ -27,8 +27,7 @@ export const MEC_core_API = {
         return hooks.hooks_onBeforeCreateMonsterUnit.new(cb)
     },
 
-    newMonsterSimplePatrol: (...args: any[]) => {
-        // @ts-ignore
+    newMonsterSimplePatrol: (...args: ConstructorParameters<typeof MonsterSimplePatrol>) => {
         return new MonsterSimplePatrol(...args)
-    }
+    },
 }
