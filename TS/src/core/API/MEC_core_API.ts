6fab131f94dff8d73df34548ea0692a2154be9ae
Max <maxime.seeweb@gmail.com>
2022-04-23 22:03:17 +0200

    API for terrain reinit

--- TS/src/core/API/MEC_core_API.ts
+++ TS/src/core/API/MEC_core_API.ts
@@ -5,6 +5,10 @@ import { makingRightsToAll } from '../06_COMMANDS/Rights/manage_rights'
 import { LoadMapFromCache } from '../07_TRIGGERS/Load_map_from_gamecache/LoadMapFromCache'
 import { hooks } from './GeneralHooks'
 import {MecHook} from "./MecHook";
+import {ReinitTerrains} from "../07_TRIGGERS/Triggers_to_modify_terrains/Reinit_terrains";
+import {
+    ReinitTerrainsPositions
+} from "../07_TRIGGERS/Triggers_to_modify_terrains/Reinit_terrains_position_Change_variations_and_ut_at_beginning";
 
 export const MEC_core_API = {
     setGameData: (jsonString: string) => {
@@ -59,4 +63,10 @@ export const MEC_core_API = {
 
         return null
     },
+
+    //terrain
+    terrain: {
+        reinitTypes: ReinitTerrains.ReinitTerrains,
+        reinitPositions: ReinitTerrainsPositions.ReinitTerrainsPosition,
+    }
 }
