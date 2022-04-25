8b236d1c821806cd513cfa7d0e484bccc2f1565d
Stannnnn <stann@live.nl>
2022-04-25 18:35:43 +0200

    Changed save location

--- core/Log/log.ts
+++ core/Log/log.ts
@@ -1,15 +1,14 @@
-import {globals} from "../../TS/globals";
-import {SyncSaveLoad} from "../../TS/src/Utils/SaveLoad/TreeLib/SyncSaveLoad";
+import { globals } from "../../TS/globals";
+import { SyncSaveLoad } from "../../TS/src/Utils/SaveLoad/TreeLib/SyncSaveLoad";
 
 export const log = (str: string) => {
-    print(str)
-    globals.logStrings.push(str)
-}
-
+  print(str);
+  globals.logStrings.push(str);
+};
 
 export const flushLogs = () => {
-    const logFile = 'MEC_log.txt'
-    const syncSaveLoad = SyncSaveLoad(true)
-    syncSaveLoad.writeFile(logFile, globals.logStrings.join("\n"))
-    print("Logs written in file " + logFile)
-}
+  const logFile = "MEC/MEC_log.txt";
+  const syncSaveLoad = SyncSaveLoad(true);
+  syncSaveLoad.writeFile(logFile, globals.logStrings.join("\n"));
+  print("Logs written in file " + logFile);
+};
