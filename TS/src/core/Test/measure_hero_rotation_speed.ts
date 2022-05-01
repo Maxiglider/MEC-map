cb8fbeedb556ad80cad75c801603b32c24b846a9
Max <maxime.seeweb@gmail.com>
2022-05-01 07:48:31 +0200

    measure precisely and apply the rotation speed of the normal sliding mode

--- TS/src/core/Test/measure_hero_rotation_speed.ts
+++ TS/src/core/Test/measure_hero_rotation_speed.ts
@@ -36,86 +36,106 @@ export const init_measureSpeed = () => {
             let curSlideTurn = 0
 
             const startBlueHero = () => {
-                let numTimer = 0
-                tBlueHero = createTimer(SLIDE_PERIOD, true, () => {
-                    numTimer += SLIDE_PERIOD
-                    const currentAngle = GetUnitFacing(hero2)
-                    const remainingDegrees = aimAngleWithSens - currentAngle
-                    if (RAbsBJ(remainingDegrees) > 0.05) {
-
-                        let diffToApplyAbs = RMinBJ(RAbsBJ(remainingDegrees), RAbsBJ(maxSlideTurnPerPeriod))
-
-                        if (diffToApplyAbs > 0.01) {
-                            //sens
-                            const sens = (remainingDegrees * maxSlideTurnPerPeriod) > 0 ? 1 : -1
-                            const maxIncreaseRotationSpeedPerPeriod = RAbsBJ(maxSlideTurnPerPeriod * SLIDE_PERIOD / HERO_ROTATION_TIME_FOR_MAXIMUM_SPEED)
-
-                            let newSlideTurn: number
-
-                            let increaseRotationSpeedPerPeriod = maxIncreaseRotationSpeedPerPeriod
-
-                            let diffToApply
-
-                            if (RAbsBJ(remainingDegrees) <= MAX_DEGREE_ON_WHICH_SPEED_TABLE_TAKES_CONTROL) {
-                                const tableInd = Math.round(RAbsBJ(remainingDegrees))
-                                const aimedSpeedPercentage = SPEED_AT_LEAST_THAN_50_DEGREES[tableInd]
-                                const aimedNewSpeedPerPeriod = maxSlideTurnPerPeriod * aimedSpeedPercentage * sens / 100
-                                const diffSpeed = aimedNewSpeedPerPeriod - curSlideTurn
-                                if (RAbsBJ(diffSpeed) < maxIncreaseRotationSpeedPerPeriod) {
-                                    // print('diff speed inferieure, aim ' + diffSpeed)
-                                    diffToApply = aimedNewSpeedPerPeriod
-                                }else{
-                                    const sensDiffToApply = diffSpeed > 0 ? 1 : -1
-                                    diffToApply = curSlideTurn + sensDiffToApply * maxIncreaseRotationSpeedPerPeriod
-                                }
-                                curSlideTurn = diffToApply
-                                // print("remainingDegrees : " + remainingDegrees + " ; tableInd : " + tableInd + " ; aim new speed : " + aimedNewSpeedPerPeriod + " ; cuSlideTurn : " + curSlideTurn + " ; maxIncrease " + maxIncreaseRotationSpeedPerPeriod + " ; diffToApply : " + diffToApply)
-
-                            }else {
-                                if (sens > 0) {
-                                    newSlideTurn = RMinBJ(curSlideTurn + increaseRotationSpeedPerPeriod, maxSlideTurnPerPeriod)
-                                    diffToApply = RMinBJ(newSlideTurn, diffToApplyAbs)
-                                    diffToApply = RMinBJ(remainingDegrees, diffToApply)
-                                } else {
-                                    newSlideTurn = RMaxBJ(curSlideTurn - increaseRotationSpeedPerPeriod, -maxSlideTurnPerPeriod)
-                                    diffToApply = RMaxBJ(newSlideTurn, -diffToApplyAbs)
-                                    diffToApply = RMaxBJ(remainingDegrees, diffToApply)
-                                }
-                                curSlideTurn = newSlideTurn
-                            }
-
-                            //turn
-                            const newAngle = currentAngle + diffToApply
-                            BlzSetUnitFacingEx(hero2, newAngle)
-                            // print("old : " + currentAngle + " ; new " + newAngle)
-
-                            log(numTimer + "\t" + GetUnitFacing(hero) + "\t" + newAngle)
-                        }
-                    }
-                })
+                // let numTimer = 0
+                // tBlueHero = createTimer(SLIDE_PERIOD, true, () => {
+                //     numTimer += SLIDE_PERIOD
+                //     const currentAngle = GetUnitFacing(hero2)
+                //     const remainingDegrees = aimAngleWithSens - currentAngle
+                //     if (RAbsBJ(remainingDegrees) > 0.05) {
+                //
+                //         let diffToApplyAbs = RMinBJ(RAbsBJ(remainingDegrees), RAbsBJ(maxSlideTurnPerPeriod))
+                //
+                //         if (diffToApplyAbs > 0.01) {
+                //             //sens
+                //             const sens = (remainingDegrees * maxSlideTurnPerPeriod) > 0 ? 1 : -1
+                //             const maxIncreaseRotationSpeedPerPeriod = RAbsBJ(maxSlideTurnPerPeriod * SLIDE_PERIOD / HERO_ROTATION_TIME_FOR_MAXIMUM_SPEED)
+                //
+                //             let newSlideTurn: number
+                //
+                //             let increaseRotationSpeedPerPeriod = maxIncreaseRotationSpeedPerPeriod
+                //
+                //             let diffToApply
+                //
+                //             if (RAbsBJ(remainingDegrees) <= MAX_DEGREE_ON_WHICH_SPEED_TABLE_TAKES_CONTROL) {
+                //                 const tableInd = Math.round(RAbsBJ(remainingDegrees))
+                //                 const aimedSpeedPercentage = SPEED_AT_LEAST_THAN_50_DEGREES[tableInd]
+                //                 const aimedNewSpeedPerPeriod = maxSlideTurnPerPeriod * aimedSpeedPercentage * sens / 100
+                //                 const diffSpeed = aimedNewSpeedPerPeriod - curSlideTurn
+                //                 if (RAbsBJ(diffSpeed) < maxIncreaseRotationSpeedPerPeriod) {
+                //                     // print('diff speed inferieure, aim ' + diffSpeed)
+                //                     diffToApply = aimedNewSpeedPerPeriod
+                //                 }else{
+                //                     const sensDiffToApply = diffSpeed > 0 ? 1 : -1
+                //                     diffToApply = curSlideTurn + sensDiffToApply * maxIncreaseRotationSpeedPerPeriod
+                //                 }
+                //                 curSlideTurn = diffToApply
+                //                 // print("remainingDegrees : " + remainingDegrees + " ; tableInd : " + tableInd + " ; aim new speed : " + aimedNewSpeedPerPeriod + " ; cuSlideTurn : " + curSlideTurn + " ; maxIncrease " + maxIncreaseRotationSpeedPerPeriod + " ; diffToApply : " + diffToApply)
+                //
+                //             }else {
+                //                 if (sens > 0) {
+                //                     newSlideTurn = RMinBJ(curSlideTurn + increaseRotationSpeedPerPeriod, maxSlideTurnPerPeriod)
+                //                     diffToApply = RMinBJ(newSlideTurn, diffToApplyAbs)
+                //                     diffToApply = RMinBJ(remainingDegrees, diffToApply)
+                //                 } else {
+                //                     newSlideTurn = RMaxBJ(curSlideTurn - increaseRotationSpeedPerPeriod, -maxSlideTurnPerPeriod)
+                //                     diffToApply = RMaxBJ(newSlideTurn, -diffToApplyAbs)
+                //                     diffToApply = RMaxBJ(remainingDegrees, diffToApply)
+                //                 }
+                //                 curSlideTurn = newSlideTurn
+                //             }
+                //
+                //             //turn
+                //             const newAngle = currentAngle + diffToApply
+                //             BlzSetUnitFacingEx(hero2, newAngle)
+                //             // print("old : " + currentAngle + " ; new " + newAngle)
+                //
+                //             log(numTimer + "\t" + GetUnitFacing(hero) + "\t" + newAngle)
+                //         }
+                //     }
+                // })
             }
 
             const stopBlueHero = () => {
                 tBlueHero?.destroy()
             }
 
-            //red hero normal rotation
-            // createTimer(0.01, true, () => {
-            //     if(!heroBlueStarted){
-            //         startBlueHero()
-            //         heroBlueStarted = true
-            //     }
-            //
-            //     const oldAngle = GetUnitFacing(hero)
-            //     const newAngle = oldAngle + 70 * sens
-            //     SetUnitFacing(hero, newAngle)
-            //
-            //     // const angleParcouru = AnglesDiff(GetUnitFacing(hero), oldAngle)
-            //     // const speed = angleParcouru / 0.5
-            //     // const turnsPerSecond = speed / 360
-            //     //
-            //     // print("angleParcouru : " + angleParcouru + " ; speed : " + speed + "° ; turnsPerSecond : " + turnsPerSecond)
-            // })
+            // red hero normal rotation
+            let lastTimeAngle0 = 0
+            let currentTime = 0
+
+
+            BlzSetUnitFacingEx(hero, 0)
+
+            createTimer(0.01, true, () => {
+                if(!heroBlueStarted){
+                    // startBlueHero()
+                    heroBlueStarted = true
+                }
+
+                const oldAngle = GetUnitFacing(hero)
+                const newAngle = oldAngle + 70 * sens
+                SetUnitFacing(hero, newAngle)
+
+                currentTime += 0.01
+                if(RAbsBJ(AnglesDiff(GetUnitFacing(hero), 0)) < 5){
+                    const circleDuration = currentTime - lastTimeAngle0
+                    if(circleDuration > 0.3) {
+                        const roundsPerSecond = 1 / circleDuration
+
+                        const str = "Angle " + GetUnitFacing(hero) + " ; anglesDiff " + AnglesDiff(GetUnitFacing(hero), 0) + " ; lastTime " + lastTimeAngle0 + " ; currentTime : " + currentTime + " ;  speed : " + roundsPerSecond
+                        print(str)
+                        log(str)
+
+                        lastTimeAngle0 = currentTime
+                    }
+                }
+
+                // const angleParcouru = AnglesDiff(GetUnitFacing(hero), oldAngle)
+                // const speed = angleParcouru / 0.5
+                // const turnsPerSecond = speed / 360
+                //
+                // print("angleParcouru : " + angleParcouru + " ; speed : " + speed + "° ; turnsPerSecond : " + turnsPerSecond)
+            })
 
             //change sens
             // createTimer(0.2, true, () => {
@@ -124,44 +144,44 @@ export const init_measureSpeed = () => {
 
             //de 10 à 150°
 
-            let aimAngle = 0
-            let aimAngleWithSens = 0
-            let pause = false
-
-            let blueStarted = false
-
-            const myTimer = createTimer(1.8, true, () => {
-
-                if (!pause) {
-                    aimAngle += 10
-
-                    if(!blueStarted){
-                        startBlueHero()
-                        blueStarted = true
-                        BlzSetUnitFacingEx(hero, 0)
-                        BlzSetUnitFacingEx(hero2, 0)
-                    }
-
-                } else {
-                    sens = - sens
-                }
-
-                aimAngleWithSens = sens * aimAngle
-
-
-                print("aim angle : " + aimAngleWithSens)
-                createTimer(0.01, false, () => {
-                    SetUnitFacing(hero, aimAngleWithSens)
-                    print("oui " + aimAngleWithSens)
-                })
-
-                pause = !pause
-
-                if (aimAngle == 150) {
-                    myTimer.destroy()
-                    stopBlueHero()
-                }
-            })
+            // let aimAngle = 0
+            // let aimAngleWithSens = 0
+            // let pause = false
+            //
+            // let blueStarted = false
+            //
+            // const myTimer = createTimer(1.8, true, () => {
+            //
+            //     if (!pause) {
+            //         aimAngle += 10
+            //
+            //         if(!blueStarted){
+            //             startBlueHero()
+            //             blueStarted = true
+            //             BlzSetUnitFacingEx(hero, 0)
+            //             BlzSetUnitFacingEx(hero2, 0)
+            //         }
+            //
+            //     } else {
+            //         sens = - sens
+            //     }
+            //
+            //     aimAngleWithSens = sens * aimAngle
+            //
+            //
+            //     print("aim angle : " + aimAngleWithSens)
+            //     createTimer(0.01, false, () => {
+            //         SetUnitFacing(hero, aimAngleWithSens)
+            //         print("oui " + aimAngleWithSens)
+            //     })
+            //
+            //     pause = !pause
+            //
+            //     if (aimAngle == 150) {
+            //         myTimer.destroy()
+            //         stopBlueHero()
+            //     }
+            // })
 
         }
 
