0af5bd8de1aca680fd65465546e36db0f60771dd
Max <maxiglider@gmail.com>
2025-12-09 21:32:43 +0100

    fix(getLogsFromMap.php): it was for base64 logs, which is no more

--- bin/getLogsFromMap.php
+++ bin/getLogsFromMap.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Cr�� avec PhpStorm.
+ * Créé avec PhpStorm.
  * Auteur: Max
  * Date: 26/03/2022
  * Heure: 00:57
@@ -8,28 +8,25 @@
 
 
 
-const DATA_FILE = "C:\SaveSurDrive\OneDrive\Documents\Warcraft III\CustomMapData\MEC\MEC_log.txt";
+const DATA_FILE = "C:\Users\maxpo\OneDrive\Documents\Warcraft III\CustomMapData\MEC\MEC_log.txt";
 const OUT_FILE = __DIR__."/MEC_outLog.txt";
 const GAME_DATA_OUT_FILE = __DIR__."/mec_game_data.txt";
 
 
 if(!is_file(DATA_FILE)){
-    die("File \"MEC_outLog.txt\" missing");
+    die("File \"MEC_log.txt\" missing");
 }
 
 
 $content = file_get_contents(DATA_FILE);
 
 
-preg_match_all('/call BlzSendSyncData\(".*?",".{16}(.+?)"/', $content, $matches, PREG_SET_ORDER);
+preg_match_all('/call Preload\( "(.+)" \)/', $content, $matches, PREG_SET_ORDER);
 
-$base64 = "";
+$uncodedContent = "";
 foreach($matches as $match) {
-    $base64 .= $match[1];
+    $uncodedContent .= $match[1];
 }
-
-$uncodedContent = base64_decode($base64);
-
 
 file_put_contents(OUT_FILE, $uncodedContent);
 
