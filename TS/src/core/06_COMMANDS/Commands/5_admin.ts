Maxiglider/MEC-map

commit b022ac0dfaa834793ebd645e681a5ccd2ca4e8b6

fix(smic/lmfc): fix bugs + erase game data before lmfc | on dev mode, no more using base64 for smic

TS/src/core/07_TRIGGERS/Load_map_from_gamecache/LoadMapFromCache.ts
@@ -1,9 +1,8 @@
-import { errorHandler } from 'Utils/mapUtils'
-import { PROD } from 'env'
 import { getUdgCasterTypes, getUdgLevels, getUdgMonsterTypes, getUdgTerrainTypes, globals } from '../../../../globals'
 import { jsonDecode } from '../../01_libraries/Basic_functions'
 import { Text } from '../../01_libraries/Text'
 import { Gravity } from '../Slide_and_CheckTerrain_triggers/Gravity'
+import { initCasterTypes, initLevels, initMonsterTypes, initTerrainTypes } from '../../Init/initArrays'
 
 export class LoadMapFromCache {
     public static gameDataJsonString: string | null = null
@@ -15,16 +14,27 @@ export class LoadMapFromCache {
             if (!gameData || typeof gameData !== 'object') {
                 Text.erA('invalid game data string')
             } else {
+                //erase previous data from the game
+                getUdgLevels().destroy()
+                getUdgCasterTypes().destroy()
+                getUdgMonsterTypes().destroy()
+                getUdgTerrainTypes().destroy()
+                initTerrainTypes()
+                initMonsterTypes()
+                initCasterTypes()
+                initLevels()
+
+                // game properties
                 if (gameData.gameData) {
-                    if (gameData.gameData.USE_VTOTO_SLIDE_LOGIC) {
+                    if (gameData.gameData.USE_VTOTO_SLIDE_LOGIC !== undefined) {
                         globals.USE_VTOTO_SLIDE_LOGIC = gameData.gameData.USE_VTOTO_SLIDE_LOGIC
                     }
 
-                    if (gameData.gameData.coopCircles) {
+                    if (gameData.gameData.coopCircles !== undefined) {
                         globals.coopCircles = gameData.gameData.coopCircles
                     }
 
-                    if (gameData.gameData.CAN_TURN_IN_AIR) {
+                    if (gameData.gameData.CAN_TURN_IN_AIR !== undefined) {
                         globals.CAN_TURN_IN_AIR = gameData.gameData.CAN_TURN_IN_AIR
                     }
 
@@ -44,47 +54,33 @@ export class LoadMapFromCache {
                         globals.wanderExtraTime = gameData.gameData.wanderExtraTime
                     }
 
-                    if (gameData.gameData.gravity) {
+                    if (gameData.gameData.gravity !== undefined) {
                         Gravity.SetGravity(gameData.gameData.gravity)
                     }
                 }
 
                 //terrain types MEC
                 if (gameData.terrainTypesMec) {
-                    if (PROD) {
-                        getUdgTerrainTypes().newFromJson(gameData.terrainTypesMec)
-                    } else {
-                        // Dont care during development
-                        errorHandler(() => getUdgTerrainTypes().newFromJson(gameData.terrainTypesMec))
-                    }
+                    getUdgTerrainTypes().newFromJson(gameData.terrainTypesMec)
                 }
 
                 //monster types
                 if (gameData.monsterTypes) {
-                    if (PROD) {
-                        getUdgMonsterTypes().newFromJson(gameData.monsterTypes)
-                    } else {
-                        // Dont care during development
-                        errorHandler(() => getUdgMonsterTypes().newFromJson(gameData.monsterTypes))
-                    }
+                    getUdgMonsterTypes().newFromJson(gameData.monsterTypes)
                 }
 
                 //caster types
                 if (gameData.casterTypes) {
-                    if (gameData.monsterTypes) {
-                        if (PROD) {
-                            getUdgCasterTypes().newFromJson(gameData.casterTypes)
-                        } else {
-                            // Dont care during development
-                            errorHandler(() => getUdgCasterTypes().newFromJson(gameData.casterTypes))
-                        }
-                    }
+                    getUdgCasterTypes().newFromJson(gameData.casterTypes)
                 }
 
                 //levels
                 if (gameData.levels) {
                     getUdgLevels().newFromJson(gameData.levels)
                 }
+
+                //restart the game to apply the changes and make mobs appear
+                getUdgLevels().restartTheGame()
             }
         }
     }

TS/src/core/04_STRUCTURES/BaseArray.ts
@@ -77,6 +77,7 @@ export abstract class BaseArray<T extends BaseModel> {
             v.destroy()
             delete this.data[id]
         }
+        this.lastInstanceId = -1;
     }
 
     // First index is 0

TS/src/core/07_TRIGGERS/Save_map_in_gamecache/SaveMapInCache.ts
@@ -72,11 +72,7 @@ export class SaveMapInCache {
 
             SaveMapInCache.lastSaveFile = filename
 
-            if (PROD) {
-                SaveLoad.saveFileWithoutPossibleLoading(filename, p, SaveMapInCache.smicStringObj.str, false)
-            } else {
-                SaveLoad.saveFile(filename, p, SaveMapInCache.smicStringObj.str, true)
-            }
+            SaveLoad.saveFileWithoutPossibleLoading(filename, p, SaveMapInCache.smicStringObj.str, false)
 
             Text.A('saving game data to file "' + filename + '" done')
 

TS/src/core/Init/initArrays.ts
@@ -19,25 +19,25 @@ const initEscapers = () => {
 
 
 //Levels
-const initLevels = () => {
+export const initLevels = () => {
     setUdgLevels(new LevelArray())
 }
 
 
 //Terrain types
-const initTerrainTypes = () => {
+export const initTerrainTypes = () => {
     setUdgTerrainTypes(new TerrainTypeArray())
 }
 
 
 //Monster types
-const initMonsterTypes = () => {
+export const initMonsterTypes = () => {
     setUdgMonsterTypes(new MonsterTypeArray())
 }
 
 
 //Caster types
-const initCasterTypes = () => {
+export const initCasterTypes = () => {
     setUdgCasterTypes(new CasterTypeArray())
 }
 

commit a4b9c0e5146137855b4dd2776ab5562083a9bb11

fix(LoadMapFromCache): broke the MEC_core hooks since commit b022ac0dfaa834793ebd645e681a5ccd2ca4e8b6

TS/src/core/06_COMMANDS/Commands/5_admin.ts
@@ -613,7 +613,7 @@ export const initExecuteCommandMax = () => {
 
                     const gameData = (jsonDecode(data) as any).gameData
 
-                    MEC_core_API.setGameData(jsonEncode(gameData))
+                    MEC_core_API.setGameData(jsonEncode(gameData), false)
                     getUdgLevels().reloadAllLevels()
                     Text.A('Done')
                 }

TS/src/core/07_TRIGGERS/Load_map_from_gamecache/LoadMapFromCache.ts
@@ -1,5 +1,5 @@
 import { getUdgCasterTypes, getUdgLevels, getUdgMonsterTypes, getUdgTerrainTypes, globals } from '../../../../globals'
-import { jsonDecode } from '../../01_libraries/Basic_functions'
+import { B2S, jsonDecode } from '../../01_libraries/Basic_functions'
 import { Text } from '../../01_libraries/Text'
 import { Gravity } from '../Slide_and_CheckTerrain_triggers/Gravity'
 import { initCasterTypes, initLevels, initMonsterTypes, initTerrainTypes } from '../../Init/initArrays'
@@ -7,22 +7,24 @@ import { initCasterTypes, initLevels, initMonsterTypes, initTerrainTypes } from 
 export class LoadMapFromCache {
     public static gameDataJsonString: string | null = null
 
-    public static initializeGameData = () => {
+    public static initializeGameData = (currentlyOnGameStart = true) => {
         if (LoadMapFromCache.gameDataJsonString) {
             const gameData: any = jsonDecode(LoadMapFromCache.gameDataJsonString)
 
             if (!gameData || typeof gameData !== 'object') {
                 Text.erA('invalid game data string')
             } else {
-                //erase previous data from the game
-                getUdgLevels().destroy()
-                getUdgCasterTypes().destroy()
-                getUdgMonsterTypes().destroy()
-                getUdgTerrainTypes().destroy()
-                initTerrainTypes()
-                initMonsterTypes()
-                initCasterTypes()
-                initLevels()
+                if (!currentlyOnGameStart) {
+                    //erase previous data from the game
+                    getUdgLevels().destroy()
+                    getUdgCasterTypes().destroy()
+                    getUdgMonsterTypes().destroy()
+                    getUdgTerrainTypes().destroy()
+                    initTerrainTypes()
+                    initMonsterTypes()
+                    initCasterTypes()
+                    initLevels()
+                }
 
                 // game properties
                 if (gameData.gameData) {
@@ -79,8 +81,10 @@ export class LoadMapFromCache {
                     getUdgLevels().newFromJson(gameData.levels)
                 }
 
-                //restart the game to apply the changes and make mobs appear
-                getUdgLevels().restartTheGame()
+                if (!currentlyOnGameStart) {
+                    //restart the game to apply the changes and make mobs appear
+                    getUdgLevels().restartTheGame()
+                }
             }
         }
     }

TS/src/core/API/MEC_core_API.ts
@@ -22,10 +22,10 @@ import { hooks } from './GeneralHooks'
 import { MecHook } from './MecHook'
 
 export const MEC_core_API = {
-    setGameData: (jsonString: string) => {
+    setGameData: (jsonString: string, currentlyOnGameStart = true) => {
         errorHandler(() => {
             LoadMapFromCache.gameDataJsonString = jsonString
-            LoadMapFromCache.initializeGameData()
+            LoadMapFromCache.initializeGameData(currentlyOnGameStart)
 
             pathingBlockerUtils.init()
             progressionUtils.init()