4a5c16391c6d21f81c3677a79620f6461880f674
Max <maxiglider@gmail.com>
2025-12-21 18:05:04 +0100

    fix(smic): getTerrainHeight was wrong on bridges

--- TS/src/core/05_MAKE_STRUCTURES/Make_get_info/MakeGetTerrainType.ts
+++ TS/src/core/05_MAKE_STRUCTURES/Make_get_info/MakeGetTerrainType.ts
@@ -1,8 +1,6 @@
 import { Make } from 'core/05_MAKE_STRUCTURES/Make/Make'
 import {Text} from "../../01_libraries/Text";
 import {GetTerrainData} from "../../07_TRIGGERS/Modify_terrain_Functions/Terrain_functions";
-import {TerrainTypeMax} from "../../07_TRIGGERS/Modify_terrain_Functions/Terrain_type_max";
-import {TerrainTypeNamesAndData} from "../../07_TRIGGERS/Modify_terrain_Functions/Terrain_type_names_and_data";
 
 export class MakeGetTerrainType extends Make {
     constructor(maker: unit) {
--- TS/src/core/07_TRIGGERS/Save_map_in_gamecache/Save_terrain.ts
+++ TS/src/core/07_TRIGGERS/Save_map_in_gamecache/Save_terrain.ts
@@ -4,7 +4,7 @@ import { Constants } from 'core/01_libraries/Constants'
 import { Text } from 'core/01_libraries/Text'
 import { TerrainType } from 'core/04_STRUCTURES/TerrainType/TerrainType'
 import { getUdgTerrainTypes, globals } from '../../../../globals'
-import { arrayPush } from '../../01_libraries/Basic_functions'
+import { arrayPush, ShowAllDestructables } from '../../01_libraries/Basic_functions'
 import { I2CustomBase64String } from '../../01_libraries/Functions_on_numbers'
 import { SaveTerrainHeights } from './Save_terrain_heights_and_cliffs'
 import { SaveTerrainRamps } from './Save_terrain_ramps'
@@ -123,9 +123,11 @@ export const PushTerrainDataIntoJson = (json: { [x: string]: any }) => {
     SaveTerrain(json) //2 MB leak
     SaveTerrainsUsed(json)
     SaveMapDimensionsAndCenterOffset(json)
+    ShowAllDestructables(false)
     SaveTerrainHeights.SaveTerrainHeights(json)
     SaveTerrainHeights.SaveTerrainCliffs(json)
     SaveTerrainRamps.SaveTerrainRamps(json)
+    ShowAllDestructables(true)
     // SaveWater.SaveWater(json) //copie from TerrainHeights for the moment ; disabled because i didn't succeed to handle water yet
 }
 
