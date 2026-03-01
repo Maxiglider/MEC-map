d2fc8cf87fbaffbb6aa9a280b9efa334a2a8e8e1
Max <maxiglider@gmail.com>
2026-03-01 15:05:12 +0100

    feat(natives): assertDefined now prints stack trace for easier debugging

--- src/core/wc3_natives_unsecured/utils.ts
+++ src/core/wc3_natives_unsecured/utils.ts
@@ -1,6 +1,6 @@
 export function assertDefined<T>(val: T | undefined, functionName: string): T {
     if (val === undefined) {
-        BJDebugMsg(`${functionName} function returned undefined`)
+        BJDebugMsg(`${functionName} function returned undefined : ` + info().GetStackTrace())
         throw new Error(`${functionName} function returned undefined`)
     }
     return val
